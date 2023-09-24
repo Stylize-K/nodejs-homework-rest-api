1620ef38ff45766df745c0d620dd93a29c24d15c
Kyrylo Lysenko <lysenko.ko@gmail.com>
2023-09-24 22:41:04 +0300

    task completed

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const app = require("./app");
 const mongoose = require("mongoose");
 const { DB_HOST, PORT = 3000 } = process.env;
 
+// Підключення до DB та запуск сервера
 mongoose
   .connect(DB_HOST)
   .then(() => {
