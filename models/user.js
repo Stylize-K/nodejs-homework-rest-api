262258550476d00c6780e6e057fba0ef4c1fb680
Kyrylo Lysenko <lysenko.ko@gmail.com>
2023-10-02 00:24:19 +0300

    step 2 part

--- models/user.js
+++ models/user.js
@@ -2,20 +2,59 @@ const { Schema, model } = require("mongoose");
 const { handleMongooseError } = require("../helpers");
 const Joi = require("joi");
 
-const userSchema = new Schema({
-  password: {
-    type: String,
-    required: [true, "Set password for user"],
+const userSchema = new Schema(
+  {
+    password: {
+      type: String,
+      required: [true, "Set password for user"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: String,
   },
-  email: {
-    type: String,
-    required: [true, "Email is required"],
-    unique: true,
-  },
-  subscription: {
-    type: String,
-    enum: ["starter", "pro", "business"],
-    default: "starter",
-  },
-  token: String,
+  { versionKey: false, timestamps: true }
+);
+
+// Надаємо статус 400 помилці валідації Mongoose, адже по дефолту методи Mongoose викидають помилки без статусу і вони прокидуються в кінці зі статусом 500
+userSchema.post("save", handleMongooseError);
+
+const registerSchema = Joi.object({
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net", "ua"] } })
+    .required()
+    .messages({
+      "any.required": "missing required email field",
+    }),
+  password: Joi.string().required().messages({
+    "any.required": "missing required password field",
+  }),
 });
+
+const loginSchema = Joi.object({
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net", "ua"] } })
+    .required()
+    .messages({
+      "any.required": "missing required email field",
+    }),
+  password: Joi.string().required().messages({
+    "any.required": "missing required password field",
+  }),
+});
+
+const schemas = {
+  registerSchema,
+  loginSchema,
+};
+
+const User = model("user", userSchema);
+
+module.exports = { User, schemas };
